056baf5099121ad3158eb86a413ca0607125cea0
hasibul.hoque <hasibul.hoque@businessaccelerate.com.bd>
2025-02-23 18:10:16 +0600

    JWT Completed with user

--- src/main/java/com/webflux/practice/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/webflux/practice/jwt/JwtAccessDeniedHandler.java
@@ -1,6 +1,5 @@
 package com.webflux.practice.jwt;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
--- src/main/java/com/webflux/practice/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/webflux/practice/jwt/JwtAuthenticationEntryPoint.java
@@ -1,12 +1,9 @@
 package com.webflux.practice.jwt;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,7 +11,7 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
         response.getWriter().write("{\"status\": 401, \"error\": \"Unauthorized\", \"message\": \"Authentication token is missing or invalid\"}");
